53c944345b3653f40147871009b9d7ff5e1ac3c1
pablopg23 <pabloparra6969@gmail.com>
2020-10-27 17:25:01 +0100

    boton cerrar
    terminado boton cerrar ayuda

--- PruebaOnlineHelp/wwwroot/js/onlineHelp.js
+++ PruebaOnlineHelp/wwwroot/js/onlineHelp.js
@@ -5,7 +5,7 @@ function loadEvents(event){
     if(document.readyState=="complete"){
         createHelp();
         document.getElementById("helpIcon").addEventListener("click", showOnlineHelp, false);
-        document.getElementById("headerDivHelp").addEventListener("click", hideOnlineHelp, false);
+        document.getElementById("closeHelp").addEventListener("click", hideOnlineHelp, false);
     }
     
 }
@@ -41,9 +41,10 @@ function createHelp() {
         divHeader.setAttribute("id","divHeader");
         var headerDivHelp = document.createElement("header");
         headerDivHelp.setAttribute("id","headerDivHelp");
-        var txtHeader = document.createTextNode("Cerrar ayuda X");
+       // var txtHeader = document.createTextNode("Cerrar ayuda X");
         
-        headerDivHelp.appendChild(txtHeader);
+        //headerDivHelp.appendChild(txtHeader);
+        divHeader.innerHTML="<svg id=\"closeHelp\" width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" class=\"bi bi-x-square-fill\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\" d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z\"/></svg>";
         divHeader.append(headerDivHelp);
         divHelp.appendChild(divHeader);
 
