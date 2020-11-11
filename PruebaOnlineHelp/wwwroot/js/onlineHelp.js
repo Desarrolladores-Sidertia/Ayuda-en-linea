ffa920e3a2f1a651310b3b10787ce1f09a92e9bf
pablopg23 <pabloparra6969@gmail.com>
2020-11-11 11:57:12 +0100

    quitar x de ayuda
    x quitada de la ayuda

--- PruebaOnlineHelp/wwwroot/js/onlineHelp.js
+++ PruebaOnlineHelp/wwwroot/js/onlineHelp.js
@@ -52,16 +52,6 @@ function createHelp() {
         var divHelp = document.createElement("div");
         divHelp.setAttribute("id","divHelp");
         
-        var divHeader = document.createElement("div");
-        divHeader.setAttribute("id","divHeader");
-        var headerDivHelp = document.createElement("header");
-        headerDivHelp.setAttribute("id","headerDivHelp");
-        divHeader.innerHTML="<svg id=\"closeHelp\" width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" class=\"bi bi-x-square-fill\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\" d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z\"/></svg>";
-        divHeader.append(headerDivHelp);
-        divHelp.appendChild(divHeader);
-
-        
-
         document.getElementsByTagName("body")[0].insertBefore(divHelp, document.getElementsByTagName("body")[0].firstChild.nextSibling);
         helpEvents();
      }
